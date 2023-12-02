7e346b63fdb5d9b5cb84f9f0e88f4b4ddf18380b
Aymaneboukdir <Aymaneboukdir639@gmail.com>
2023-12-02 11:50:05 -0800

    addfirst4 files

---
+++ 2string.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns the length of a stringg
+ * @s: the string whose length to checkk
+ *
+ * Return: integer length of stringg
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison of two strangss.
+ * @s1: the first strangg
+ * @s2: the second strang
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to searchh
+ * @needle: the substring to findd
+ *
+ * Return: address of next char of haystack 
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
---
+++ 2string1.c
@@ -0,0 +1,106 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies a stringg
+ * @dest: the destinationn
+ * @src: the sourcee
+ *
+ * Return: pointer to destinationn
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - dduplicates a string
+ * @str: string to duplicate
+ *
+ * Return: ppointer to duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints input stringg
+ * @str: the string to be printedd
+ *
+ * Return: Nothingg
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the characterr c to stdout
+ * @c: The character to printt
+ *
+ * Return: On success 1
+ * On error, -1 is returned,  errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+/**
+ * _strcat - concatenates two stringss
+ * @dest: the destination bufferr
+ * @src: the source bufferr
+ *
+ * Return: pointer to destination bufferrr
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
+
